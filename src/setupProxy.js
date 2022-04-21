4479d576ddb883f5760bed43f4ac66540e1012dc
Jacob <jacobmolsby@hotmail.se>
2022-04-21 13:25:33 +0200

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://triv-rest.herokuapp.com',
+      target: 'https://triv-rest.herokuapp.com/api/trivia/',
       changeOrigin: true,
     })
   );
